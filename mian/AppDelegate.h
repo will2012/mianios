7cf2e5e56b64723e7e222d7e3f772d5dc3c5c998
will <will.song@laba.cn>
2015-05-28 23:18:11 +0800

    initialize repo

---
+++ mian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mian
+//
+//  Created by will.song on 15/5/28.
+//  Copyright (c) 2015年 wc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UINavigationController *navController;
+
+@end
+
---
+++ mian/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  mian
+//
+//  Created by will.song on 15/5/28.
+//  Copyright (c) 2015年 wc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
